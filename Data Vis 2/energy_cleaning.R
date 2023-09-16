4eeb6ddbd3595543806c3e2ccaec1af9afef684e
bened_wards <ben.cayde.edwards@gmail.com>
2023-09-17 09:26:33 +1000

    altered data

--- Data Vis 2/energy_cleaning.R
+++ Data Vis 2/energy_cleaning.R
@@ -1,10 +1,14 @@
 library(tidyverse)
 
 energy <- read.csv("data/owid-energy-data.csv")
+map_df <- fromJSON("data/ne_110m_admin_0_countries.topojson")
 
 nas <- energy |> summarise(across(everything(), ~ sum(is.na(.)))) |> 
   pivot_longer(population:wind_share_energy, names_to="col", values_to="no_na")
 
 energy_clean <- energy |> select(country, year, iso_code, population, oil_production, coal_production)
- 
+
 solar_share <- energy |> select(country, year, iso_code, solar_share_elec, solar_share_energy)
+
+solar_share <- read.csv("data/share-electricity-solar.csv") |> filter(Year == 2022) |> rename(`solar_perc` = `Solar....electricity.`)
+write.csv(solar_share, "data/share-elec-solar-2022.csv")
