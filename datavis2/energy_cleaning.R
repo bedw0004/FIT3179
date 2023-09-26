3a6d71a49d3148f7a5d08ee25f64f2935e354d47
bened_wards <ben.cayde.edwards@gmail.com>
2023-09-26 19:48:58 +1000

    filtered countries share low carbon

--- datavis2/energy_cleaning.R
+++ datavis2/energy_cleaning.R
@@ -3,6 +3,10 @@ library(tidyverse)
 energy <- read.csv("data/owid-energy-data.csv")
 map_df <- fromJSON("data/ne_110m_admin_0_countries.topojson")
 
+interesting_countries <- c("World", "Australia", "Brazil", "China", "India", "Russia", "United States", 
+                           "Germany", "France", "United Kingdom", "Norway", "Canada", "Sweden", "Japan", 
+                           "South Africa")
+
 valid_countries <- append(read.csv("data/country_info.csv")$name, "United States")
 
 nas <- energy |> summarise(across(everything(), ~ sum(is.na(.)))) |> 
@@ -41,7 +45,7 @@ elec_prod_by_source <- read.csv("data/electricity_prod_source_stacked.csv")
 oldnames <- colnames(elec_prod_by_source)[4:12]
 newnames <- c("other_renewables", "bioenergy", "solar", "wind", "hydro", "nuclear", "oil", "gas", "coal")
 elec_prod_by_source <- elec_prod_by_source |> rename_at(vars(oldnames), ~ newnames) |> 
-  filter(Entity %in% c("World", "Australia", "Brazil", "China", "India", "Russia", "United States", "Germany", "France", "United Kingdom", "Norway", "Canada", "Sweden", "Japan", "South Africa"))
+  filter(Entity %in% interesting_countries)
 
 elec_prod_by_source_rank <- elec_prod_by_source |> 
   pivot_longer(cols=other_renewables:coal, names_to="source", values_to = "energy_twh") |> 
@@ -59,7 +63,9 @@ elec_prod_by_source <- elec_prod_by_source |>
   mutate(renewables = other_renewables + bioenergy + solar + wind + hydro,
          fossil_fuels = coal + oil + gas) |> 
   select(-other_renewables, -bioenergy, -solar, -wind, -hydro, -coal, -oil, -gas) |> 
-  pivot_longer(cols=nuclear:fossil_fuels, names_to="source", values_to = "energy_twh")
+  pivot_longer(cols=nuclear:fossil_fuels, names_to="source", values_to = "energy_twh") |> 
+  group_by(Year, Entity) |> 
+  mutate(energy_prop = energy_twh / sum(energy_twh))
 
 write.csv(elec_prod_by_source, "data/elec_prod_by_source.csv")
 
@@ -123,9 +129,18 @@ solar_share_imputed <- solar_share |>
   pivot_longer(cols=`2000`:`1999`, names_to="Year", values_to="solar_perc")
 # write.csv(solar_share_imputed, "data/share-elec-solar.csv")
 
-solar_share_2022 <- read.csv("data/share-electricity-solar.csv") |> filter(Year == 2022) |> rename(`solar_perc` = `Solar....electricity.`)
-write.csv(solar_share_2022, "data/share-elec-solar-2022.csv")
+######## SHARE OF LOW CARBON ENERGY
+low_carbon_share <- read.csv("data/share-electricity-low-carbon.csv") |> 
+  rename(low_carbon_perc = `Low.carbon.electricity....electricity.`) |> 
+  pivot_wider(names_from=Year, values_from=low_carbon_perc) |> 
+  pivot_longer(cols=`2000`:`1999`, names_to="Year", values_to="low_carbon_perc") |> 
+  group_by(Entity) |> 
+  arrange(Year) |> 
+  fill(low_carbon_perc, .direction='down')
 
-solar_share_2021 <- read.csv("data/share-electricity-solar.csv") |> filter(Year == 2021) |> rename(`solar_perc` = `Solar....electricity.`)
-write.csv(solar_share_2021, "data/share-elec-solar-2021.csv")
+write.csv(low_carbon_share, "data/share_elec_low_carbon.csv")
 
+low_carbon_share_filtered <- low_carbon_share |> 
+  filter(Entity %in% interesting_countries)
+
+write.csv(low_carbon_share_filtered, "data/share_elec_low_carbon_filtered.csv")
